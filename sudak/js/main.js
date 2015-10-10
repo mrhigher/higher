c5ba812b037f25bb4567954aa674b336dbb85448
muriton <muriton@mail.ru>
2015-10-10 12:37:47 +0300

    Merge origin/master
    Conflicts:
    	verstka/js/main.js

--- sudak/js/main.js
+++ sudak/js/main.js
@@ -1,56 +1,5 @@
 jQuery(document).ready(function() {
-    // меню
-
-    var ww = document.body.clientWidth;
-
-    $(document).ready(function() {
-        $(".nav li a").each(function() {
-            if ($(this).next().length > 0) {
-                $(this).addClass("parent");
-            };
-        })
-
-        $(".toggleMenu").click(function(e) {
-            e.preventDefault();
-            $(this).toggleClass("active");
-            $(".nav").toggle();
-        });
-        adjustMenu();
-    })
-
-    $(window).bind('resize orientationchange', function() {
-        ww = document.body.clientWidth;
-        adjustMenu();
-    });
-
-    var adjustMenu = function() {
-        if (ww < 734) {
-            $(".toggleMenu").css("display", "inline-block");
-            if (!$(".toggleMenu").hasClass("active")) {
-                $(".nav").hide();
-            } else {
-                $(".nav").show();
-            }
-            $(".nav li").unbind('mouseenter mouseleave');
-            $(".nav li a.parent").unbind('click').bind('click', function(e) {
-                // must be attached to anchor element to prevent bubbling
-                e.preventDefault();
-                $('.hover').not($(this).parent("li")).removeClass('hover ico_top');
-                $(this).parent("li").toggleClass("hover ico_top");
-            });
-        } 
-        else if (ww >= 734) {
-            $(".toggleMenu").css("display", "none");
-            $(".nav").show();
-            $(".nav li").removeClass("hover");
-            $(".nav li a").unbind('click');
-            $(".nav li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
-                // must be attached to li so that mouseleave is not triggered when hover over submenu
-                $(this).toggleClass('hover');
-            });
-        }
-    }
-    
+ 
     var slider = $('#wrap_for_bx_1 .bxslider').bxSlider({
         mode: 'fade',
         pager: false,
@@ -74,12 +23,50 @@ jQuery(document).ready(function() {
       return false;
     });
     
-    $('#bx_2 .bxslider').bxSlider({
-        minSlides: 3,
-        maxSlides: 5,
-//        slideMargin: 60
-        pager: false
-    });
+    // popup
+    $('.show_popup').click(function(e) {
+        e.preventDefault();
+        $('.overlay, .order_btn').fadeIn(500);
+		positionTop();
+        positionWidth();
+	});
+	
+	$('.close_popup').click(function() {
+		$('.overlay, .order_btn').fadeOut(500);
+	});
+    
+    $('.overlay').click(function() {
+		$('.overlay, .order_btn').fadeOut(500);
+	});
     
 });
+
+$(window).load(function() {
+  $('#flexslider').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 110,
+    itemMargin: 5,
+      minItems: 3,
+    maxItems: 4
+  });
+});
+
+$(document).keydown(function(e) {
+    // ESCAPE key pressed
+    if (e.keyCode == 27) {
+        $(".order_btn, .overlay").fadeOut(500);
+    }
+});
+   
+	
+function positionTop() {
+    var heightPopup = $('.order_btn').outerHeight() / 2;
+    $('.order_btn').css({'marginTop': '-' +  heightPopup + 'px'});
+}
+
+function positionWidth() {
+    var widthPopup = $('.order_btn').outerWidth() / 2;
+    $('.order_btn').css({'marginLeft': '-' +  widthPopup + 'px'});
+}
 
