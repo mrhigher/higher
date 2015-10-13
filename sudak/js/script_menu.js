db73229ef7dce2fef688077b0e2f5931eeec83fb
muriton <muriton@mail.ru>
2015-10-13 12:17:24 +0300

    Merge origin/master
    Conflicts:
    	verstka/js/main.js

--- sudak/js/script_menu.js
+++ sudak/js/script_menu.js
@@ -23,66 +23,41 @@ $(document).ready(function() {
 	adjustMenu();
     
     /*******************************************************/
-    if(ww < 1022) {
-        $('.menu_left h3').mouseenter(function() {
-            $('.catalogmenu').css('display', 'block');
-        });
-        $('.catalogmenu').mouseleave(function() {
-            $(this).hide();
-        });
-        $('.menu_left h3').click(function() {
-            $('.catalogmenu').css('display', 'block');
-        });
-    }
+
     
 })
-/*
-$(window).bind('resize orientationchange', function() {
-	ww = document.body.clientWidth;
-	adjustMenu();
-});*/
 
 var adjustMenu = function() {
-	
-    if (ww < 1022) {
-		//$(".toggleMenu").css("display", "inline-block");
-		/*if (!$(".toggleMenu").hasClass("active")) {
-			$(".catalogmenu").hide();
-		} else {
-			$(".catalogmenu").show();
-		}*/
-		$(".catalogmenu li").unbind('mouseenter mouseleave');
-		$(".catalogmenu li a.parent").unbind('click').bind('click', function(e) {
-			// Необходимоо привязать к элементу ссылки для предотвращения "всплывания"
+    
+    
+$(window).bind('orientationchange', function() {
+     
+	location.reload();
+});
+
+        checkSize();
+    
+      $(window).resize(checkSize);
+      function checkSize(){
+        if ($(".catalogmenu").css("z-index") == "2" ){
+            
+            $(".catalogmenucolumn").unbind('click').bind('click', function(e) {
 			e.preventDefault();
-			$(this).parent("li").toggleClass("hover"); 
-                                       
-		});
-	} 
-	else  if (ww >= 1) {
-		//$(".toggleMenu").css("display", "none");
-		//$(".catalogmenu").show();
-		$(".catalogmenu li").removeClass("hover");
-		$(".catalogmenu li a").unbind('click');
-		$(".catalogmenu li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
-		 	// Необходимо привязать к элементу li для предотвращения запуска события mouseleave при перемещении курсора мыши над подменю
-                        
-                    $(this).toggleClass('hover');
-                    /*detect if ul exists*/
-//                        if($(this).find('ul').length) {
-//                            $(this).toggleClass('hasDD');
-//                        }
-                         /*_flag detect if ul exists end*/
-                        
-		});
-                
-                
-                                   
-                                    
-                
-                
-	}
+                        $(".catalogmenu").toggle();
+            });
+            
+        }   else {
+            $('.catalogmenu').show();
+            $(".catalogmenu li").removeClass("hover");
+//		$(".catalogmenu li a").unbind('click');
+//		$(".catalogmenu li").unbind('mouseleave').bind('mouseenter mouseleave', function() {
+//                    $(this).toggleClass('hover');
+//		});
         
-      
+          
+            }
+        }
+    
+          
 }
 
