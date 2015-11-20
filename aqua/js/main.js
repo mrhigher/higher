98f937067a86048085bd6a9d4330632f70abdb85
vit <vit@DESKTOP-A9LAQA4>
2015-11-20 10:36:30 +0300

    Merge origin/master
    Conflicts:
    	aqua/css/style.css
    	aqua/index.html

--- aqua/js/main.js
+++ aqua/js/main.js
@@ -1,19 +1,11 @@
     $(document).ready(function(){
-    var $arrows = $('.arrows');
-    var $next = $arrows.children(".slick-next");    
-    var $prev = $arrows.children(".slick-prev");  
+ 
      var slick = $('.slick_slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         centerMode: true,
         variableWidth: true,
     });
-      $('.slick-next').on('click', function (e) {
-       slick.slick('slickNext');
-    });
-    $('.slick-prev').on('click', function (e) {
-        slick.slick('slickPrev');
-    });
- 
+
      
     });
