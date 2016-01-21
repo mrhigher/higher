18f934aae27c6e944e7890ab381512747268b7b7
muriton <muriton@mail.ru>
2016-01-21 16:03:32 +0200

    Merge origin/master
    Conflicts:
    	aqua/css/style.css
    	aqua/page3.html

--- gosp/app/js/common.js
+++ gosp/app/js/common.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 	$('.bxslider').bxSlider({
+		auto: true,
+		autoHover: true,
 		controls: false,
 		slideWidth: 480,
-		speed: 500,
+		pause: 0,
+		speed: 4000,
 		minSlides: 2,
 		maxSlides: 3,
 		mode: 'horizontal'
