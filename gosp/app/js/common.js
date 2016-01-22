a5311c08db4aaeba6220528e0e0f0ee0b14ef9f4
muriton <muriton@mail.ru>
2016-01-22 17:49:04 +0200

    Merge origin/master
    Conflicts:
    	aqua/css/style.css
    	aqua/page3.html

--- gosp/app/js/common.js
+++ gosp/app/js/common.js
@@ -1,22 +1,31 @@
 $(document).ready(function(){
 	
-	$('#top_slider').slick({
-		autoplay: true,
-		arrows: false,
-		variableWidth: true,
-		slidesToShow: 4,
-		autoplaySpeed: 4500
-		//rows: 3
-	})
+//	$('#top_slider').slick({
+//		autoplay: true,
+//		arrows: false,
+//		variableWidth: true,
+//		slidesToShow: 4,
+//		autoplaySpeed: 4500
+//		//rows: 3
+//	})
+	
+	$('#top_slider').bxSlider({
+		auto: true,
+		controls: false,
+		speed: 300,
+		slideWidth: 480,
+		maxSlides: 6
+		//slideWidth: 480
+	});
 	
 	$('.ul_4 li').matchHeight();
 	
-	$('.block_news').slick({
-		autoplay: true,
-		arrows: false,
-		variableWidth: true,
-		slidesToShow: 3,
-		autoplaySpeed: 4500
-		//rows: 3
-	})
+//	$('.block_news').slick({
+//		autoplay: true,
+//		arrows: false,
+//		variableWidth: true,
+//		slidesToShow: 3,
+//		autoplaySpeed: 4500
+//		//rows: 3
+//	})
 });
