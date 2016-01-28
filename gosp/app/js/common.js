75dd0fd47313b6722b701bf5b6c7c45a400f9467
mrhigher <mrhigher@mail.ru>
2016-01-28 17:11:26 +0300

    Merge origin/master
    Conflicts:
    	gosp/app/index.html

--- gosp/app/js/common.js
+++ gosp/app/js/common.js
@@ -66,97 +66,8 @@ jQuery('.vid_treiler ._face, .vid_treiler .play_ic').click(function(){
 
 });
 
-/***/
-
-
-$(document).ready(function () {
-		// initiates gallery slider			
-		var TSsettings = function() {
-			var TSsettings1 = {
-				infiniteLoop: true,
-				controls: false,
-				arrows: false,
-				variableWidth: true,
-				auto:true,
-				slidesToShow: 3,
-				minSlides: 3,
-				maxSlides: 4,
-				moveSlides: 1,
-				startSlide: 0,
-				mode: 'vertical',
-                            
-			};
-			var TSsettings2 = {
-				infiniteLoop: true,
-				controls: false,
-				arrows: false,
-				variableWidth: true,
-				auto:true,
-				slidesToShow: 3,
-				maxSlides: 10,
-				moveSlides: 1,
-				startSlide: 0,
-				slideWidth: 480
-			};
-			return ($(window).width()<530) ? TSsettings1 : TSsettings2;
-		}
-
-		var TSSlider;
-
-		function TStourLandingScript() {
-			TSSlider.reloadSlider(TSsettings());
-		}
-
-		TSSlider = $('#top_slider').bxSlider(TSsettings());
-		$(window).resize(TStourLandingScript);
-
-		
-	});
-        
-        
-        $(document).ready(function () {
-		// initiates news slider			
-		var BNsettings = function() {
-			var BNsettings1 = {
-                            pager:false,
-                            controls: false,
-                            arrows: false,
-                  	    variableWidth: true,
-                            auto:true,
-                            slidesToShow: 3,
-                            minSlides: 3,
-                            maxSlides: 4,
-                            moveSlides: 1,
-                            startSlide: 0,
-                            mode: 'vertical',
-//                            adaptiveHeight: true
-			};
-			var BNsettings2 = {
-                            pager:false,
-                            controls: false,
-                            arrows: false,
-                  	    variableWidth: true,
-                            auto:true,
-                            slidesToShow: 3,
-                            maxSlides: 10,
-                            moveSlides: 1,
-                            startSlide: 0,
-                            slideWidth: 329
-			};
-			return ($(window).width()<530) ? BNsettings1 : BNsettings2;
-		}
-
-		var BNSlider;
-
-		function BNtourLandingScript() {
-			BNSlider.reloadSlider(BNsettings());
-		}
-
-		BNSlider = $('.block_news').bxSlider(BNsettings());
-		$(window).resize(BNtourLandingScript);
-
-		
-	});
+      
+     
 
 $(document).keydown(function(e) {
     // ESCAPE key pressed
@@ -185,6 +96,7 @@ jQuery.fn.center = function () {
                                                 $(window).scrollLeft()) + "px");
     return this;
 }
+
 jQuery.fn.fixedHeader = function () {
       var window_width = $(window).width();
         if ($(window).scrollTop() >= 5 && window_width > 699) {
