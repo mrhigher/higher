fd5ae3dede0afcd9895d96dd5c136ff5cab5b1ea
mrhigher <mrhigher@mail.ru>
2016-02-02 11:03:02 +0300

    Merge origin/master
    Conflicts:
    	gosp/app/style.css

--- gosp/app/libs/menu/script_menu.js
+++ gosp/app/libs/menu/script_menu.js
@@ -13,7 +13,7 @@ var adjustMenu = function() {
 		if (!container.is(e.target) // if the target of the click isn't the container...
 			&& container.has(e.target).length === 0) // ... nor a descendant of the container
 		{
-                            $('.flexnav').removeClass('flexnav-show');
+                            //$('.flexnav').removeClass('flexnav-show');
 
 		}
 	});
