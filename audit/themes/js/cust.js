156b7bfab7468c077a8d35392195c73f35b1d041
bipeo <katamason1990@gmail.com>
2016-03-02 19:17:34 +0300

    Merge origin/master
    Conflicts:
    	gosp/app/style.css

---
+++ audit/themes/js/cust.js
@@ -0,0 +1,8 @@
+$(function(){
+	
+	$('.phone_mask').mask('+7 (000) 000-00-00');
+
+	//$.pixlayout({clip: true, src: "maket.png", show:true, center:true, top:0, left:0, pervious: false});
+
+});
+
---
+++ itklaster/themes/slick/custom/script.js
@@ -0,0 +1,12 @@
+$(function(){
+	var slick = $('.slick_slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		centerMode: true,
+		variableWidth: true,
+		dots:false,
+                autoplay: true,
+	});
+});
+
+
